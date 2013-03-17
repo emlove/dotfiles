42385b9f3a5c611e320d04e3dfc18ef35b0f3b90
Adam Mills <adam@armills.info>
2013-03-17 18:37:04 -0400

    Updated uzbl dmenu colors

--- uzbl/scripts/util/dmenu.sh
+++ uzbl/scripts/util/dmenu.sh
@@ -3,3 +3,7 @@
 DMENU_LINES=30
 
 . "/usr/share/uzbl/examples/data/scripts/util/dmenu.sh"
+
+DMENU_COLORS="-nb #222222 -nf #888888 -sb #285577 -sf #ffffff"
+
+DMENU="dmenu $DMENU_ARGS $DMENU_COLORS"

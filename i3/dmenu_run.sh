ebf2a99c66a07d2a7f0ec534206f7b0dec7f4737
Adam Mills <adam@armills.info>
2013-03-30 15:13:45 -0400

    More theme adjustments

---
+++ i3/dmenu_run.sh
@@ -0,0 +1,1 @@
+dmenu_run -i -fn "Ubuntu Sans-8:normal" -sf '#eeeeee' -sb '#0771a6' -nf '#888888' -nb '#222222'
--- uzbl/scripts/util/dmenu.sh
+++ uzbl/scripts/util/dmenu.sh
@@ -2,8 +2,10 @@
 
 DMENU_LINES=30
 
+DMENU_FONT="UbuntuSans-8:normal"
+
 . "/usr/share/uzbl/examples/data/scripts/util/dmenu.sh"
 
-DMENU_COLORS="-nb #222222 -nf #888888 -sb #285577 -sf #ffffff"
+DMENU_COLORS="-nb #222222 -nf #888888 -sb #0771a6 -sf #eeeeee"
 
 DMENU="dmenu $DMENU_ARGS $DMENU_COLORS"

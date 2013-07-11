bfb27346a1237cb9130a3b922236f69b77b3a74c
Adam Mills <adam@armills.info>
2013-07-10 23:09:25 -0400

    color theme reorganization

--- i3/reload-colors.sh
+++ i3/reload-colors.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cat ~/.i3/[0-9][0-9]* >~/.i3/config
+cat ~/dotfiles/i3/[0-9][0-9]* >~/dotfiles/i3/config
 for f in ~/.Xresources/*
 do
     xrdb -merge $f
 done
+
+cat ~/dotfiles/i3status/[0-9][0-9]* > ~/dotfiles/i3status/i3status.conf

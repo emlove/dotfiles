7e002706f07d314a1e0f13c53b0e83dcafcae75f
Adam Mills <adam@armills.info>
2013-07-23 22:43:18 -0400

    Reorganized reload-colors script

--- i3/reload-colors.sh
+++ i3/reload-colors.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+# Merge config files that needed to be seperated
 cat ~/dotfiles/i3/[0-9][0-9]* >~/dotfiles/i3/config
+cat ~/dotfiles/i3status/[0-9][0-9]* > ~/dotfiles/i3status/i3status.conf
+cat ~/dotfiles/uzbl/[0-9][0-9]* > ~/dotfiles/uzbl/config
+
+# Apply all files in .Xresources
 for f in ~/.Xresources/*
 do
     xrdb -merge $f
 done
 
-cat ~/dotfiles/i3status/[0-9][0-9]* > ~/dotfiles/i3status/i3status.conf
-
-cat ~/dotfiles/uzbl/[0-9][0-9]* > ~/dotfiles/uzbl/config
+# Apply wallpaper
+feh --no-xinerama --bg-center '/home/adam/dotfiles/color-themes/current-theme/wallpaper.png'
